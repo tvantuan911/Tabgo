49509a15eea1adea6f40815d2f91afb2794548ad
Tuan Truong <tvantuan911@gmail.com>
2025-02-28 12:24:20 +0700

    Xây dựng tính năng ghi nhớ Tab

--- src/tabswitch.js
+++ src/tabswitch.js
@@ -1,8 +1,10 @@
-function TabSwitch(selector) {
+function TabSwitch(selector, options = {}) {
     this.container = document.querySelector(selector);
 
-    if(!this.container) {
-        console.error(`Tabswitch: No container found for selector '${selector}'`);
+    if (!this.container) {
+        console.error(
+            `Tabswitch: No container found for selector '${selector}'`
+        );
         return;
     }
 
@@ -13,35 +15,54 @@ function TabSwitch(selector) {
         return;
     }
 
-    this.panels = this.tabs.map(tab => {
-        const panel = document.querySelector(tab.getAttribute("href"));
-        if (!panel) {
-            console.error(`Tabswitch: No panel foud for selector '${tab.getAttribute("href")}'`);
-        }
-        return panel;
-    }).filter(Boolean);
+    this.panels = this.tabs
+        .map((tab) => {
+            const panel = document.querySelector(tab.getAttribute("href"));
+            if (!panel) {
+                console.error(
+                    `Tabswitch: No panel foud for selector '${tab.getAttribute(
+                        "href"
+                    )}'`
+                );
+            }
+            return panel;
+        })
+        .filter(Boolean);
 
     if (this.panels.length !== this.tabs.length) return;
+
+    this.opt = Object.assign({
+        remember: false,
+    }, options);
 
     this._originalHTML = this.container.innerHTML;
 
     this._init();
 }
 
-Tabswitch.prototype._init = function() {
-    this._activateTab(this.tabs[0]);
+Tabswitch.prototype._init = function () {
+    const hash = location.hash;
+    // if (hash) {
+    //     tabToActivate = this.tabs.find(tab => tab.getAttribute('href') === hash) || this.tabs[0];
+    // } else {
+    //     tabToActivate = this.tabs[0];
+    // }
 
-    this.tabs.forEach(tab => {
+    const tabToActivate =
+        (this.opt.remember && hash && this.tabs.find((tab) => tab.getAttribute("href") === hash)) ||
+        this.tabs[0];
+
+    this._activateTab(tabToActivate);
+
+    this.tabs.forEach((tab) => {
         tab.onclick = (event) => this._handleTabClick(event, tab);
     });
-    
-}
+};
 
-
-Tabswitch.prototype._handleTabClick = function(event, tab) {
+Tabswitch.prototype._handleTabClick = function (event, tab) {
     event.preventDefault();
     this._activateTab(tab);
-}
+};
 
 Tabswitch.prototype._activateTab = function (tab) {
     this.tabs.forEach((tab) => {
@@ -53,13 +74,20 @@ Tabswitch.prototype._activateTab = function (tab) {
     this.panels.forEach((panel) => (panel.hidden = true));
     const panelActive = document.querySelector(tab.getAttribute("href"));
     panelActive.hidden = false;
-}
+
+    // localStorage.setItem('tabswitch-active', tab.getAttribute('href'));
+    if (this.opt.remember) {
+        history.replaceState(null, null, tab.getAttribute("href"));
+    }
+};
 
 Tabswitch.prototype.switch = function (input) {
     let tabToActivate = null;
 
-    if (typeof input === 'string') {
-        tabToActivate = this.tabs.find(tab => tab.getAttribute('href') === input);
+    if (typeof input === "string") {
+        tabToActivate = this.tabs.find(
+            (tab) => tab.getAttribute("href") === input
+        );
         if (!tabToActivate) {
             console.error(`Tabswitch: No panel found with ID '${input}'`);
             return;
@@ -74,14 +102,13 @@ Tabswitch.prototype.switch = function (input) {
     }
 
     this._activateTab(tabToActivate);
-}
+};
 
 Tabswitch.prototype.destroy = function () {
     this.container.innerHTML = this._originalHTML;
 
-    this.panels.forEach(panel => panel.hidden = false);
+    this.panels.forEach((panel) => (panel.hidden = false));
     this.container = null;
     this.tabs = null;
     this.panels = null;
-}
-
+};
